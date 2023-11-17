9b95b3b090ad232afc178daf9bff4356cb4b4d46
Cao Bittencourt <caobittencourt@gmail.com>
2023-11-17 18:32:06 -0300

    scale upper bound

--- R/fun_comp.R
+++ R/fun_comp.R
@@ -72,8 +72,12 @@ fun_comp_competence <- function(
   )
   
   stopifnot(
-    "'dbl_scale_ub' must be numeric." =
-      is.numeric(dbl_scale_ub)
+    "'dbl_scale_ub' must be numeric and greater than 'dbl_scale_lb'." =
+      all(
+        is.numeric(dbl_scale_ub)
+        , dbl_scale_ub > 
+          dbl_scale_lb
+      )
   )
   
   stopifnot(
@@ -89,21 +93,23 @@ fun_comp_competence <- function(
   
   dbl_scale_ub[[1]] -> dbl_scale_ub
   
+  dbl_profile[!is.na(
+    dbl_profile
+  )] -> dbl_profile
+  
   if(is.null(dbl_generality)){
     
     fun_gene_generality(
       dbl_profile
       , dbl_scale_lb =
         dbl_scale_lb
+      , dbl_scale_ub = 
+        dbl_scale_ub
     ) -> dbl_generality
     
   }
   
   dbl_generality[[1]] -> dbl_generality
-  
-  dbl_profile[!is.na(
-    dbl_profile
-  )] -> dbl_profile
   
   # Weighted mean of normalized item scores
   # adjusted by item importance and generality
